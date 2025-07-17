48282937d24b53331236ce8e433f4466197ee267
Swarupanand Popat Bajbalkar <151636990+Swarupanand05@users.noreply.github.com>
2025-07-17 22:25:57 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,15 @@
+package com.dkte;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		int [] arr= {1,2,3,4};
+		int i=0;
+		for(i=arr.length-1;i>=0;i--)
+		{
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
